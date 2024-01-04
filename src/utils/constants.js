3b870df8e6b46e2014cb030a99b58a1ebf611069
premnarayanp <premnarayanpatel223@gmail.com>
2024-01-04 14:50:06 +0530

    Add Custom fetch for API call in api and add contant URLs to fetch API and LocalStorage to store and remove JWT Token

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const API_ROOT = 'https://dummyjson.com';
+
+export const API_URLS = {
+  //API URL for User
+  login: () => `${API_ROOT}/auth/login`,
+  signup: () => `${API_ROOT}/auth/login`,
+
+
+};
+
+export const LOCAL_STORAGE_TOKEN_KEY = '__MY_CART_APP_TOKEN__';
